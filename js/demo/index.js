b9c16365a64074153f515ad3da3caac06ceb010d
Nicolas Donati <112676468+NDonatti@users.noreply.github.com>
2024-07-31 12:16:48 -0600

    scroll

--- js/demo/index.js
+++ js/demo/index.js
@@ -10,31 +10,31 @@ const totalContentElements = contentElements.length;
 
 // Initializes Lenis for smooth scrolling with specific properties
 const initSmoothScrolling = () => {
-	// Instantiate the Lenis object with specified properties
-	lenis = new Lenis({
-		lerp: 0.05, // Lower values create a smoother scroll effect
-		smoothWheel: true // Enables smooth scrolling for mouse wheel events
-	});
+    // Instantiate the Lenis object with specified properties
+    lenis = new Lenis({
+        lerp: 0.05, // Lower values create a smoother scroll effect
+        smoothWheel: true // Enables smooth scrolling for mouse wheel events
+    });
 
-	// Update ScrollTrigger each time the user scrolls
-	lenis.on('scroll', () => ScrollTrigger.update());
+    // Update ScrollTrigger each time the user scrolls
+    lenis.on('scroll', () => ScrollTrigger.update());
 
-	// Define a function to run at each animation frame
-	const scrollFn = (time) => {
-		lenis.raf(time); // Run Lenis' requestAnimationFrame method
-		requestAnimationFrame(scrollFn); // Recursively call scrollFn on each frame
-	};
-	// Start the animation frame loop
-	requestAnimationFrame(scrollFn);
+    // Define a function to run at each animation frame
+    const scrollFn = (time) => {
+        lenis.raf(time); // Run Lenis' requestAnimationFrame method
+        requestAnimationFrame(scrollFn); // Recursively call scrollFn on each frame
+    };
+    // Start the animation frame loop
+    requestAnimationFrame(scrollFn);
 };
 
 // Function to handle scroll-triggered animations
 const scroll = () => {
 
     contentElements.forEach((el, position) => {
-        
-		const isLast = position === totalContentElements-1;
-		
+
+        const isLast = position === totalContentElements - 1;
+
         gsap.timeline({
             scrollTrigger: {
                 trigger: el,
@@ -43,29 +43,29 @@ const scroll = () => {
                 scrub: true
             }
         })
-        .to(el, {
-			ease: 'none',
-            yPercent: -100
-        }, 0)
-		// Animate the content inner image
-        .fromTo(el.querySelector('.content__img'), {
-			yPercent: 20,
-            rotation: 40,
-			scale: 0.8,
-			filter: 'contrast(150%)'
-		}, {
-            ease: 'none',
-            yPercent: -100,
-            rotation: 0,
-			scale: 1,
-			filter: 'contrast(100%)',
-			scrollTrigger: {
-                trigger: el,
-                start: 'top bottom',
-                end: 'max',
-                scrub: true
-            }
-        }, 0);
+            .to(el, {
+                ease: 'none',
+                yPercent: -100
+            }, 0)
+            // Animate the content inner image
+            .fromTo(el.querySelector('.content__img'), {
+                yPercent: 20,
+                rotation: 40,
+                scale: 0.8,
+                filter: 'contrast(150%)'
+            }, {
+                ease: 'none',
+                yPercent: -100,
+                rotation: 0,
+                scale: 1,
+                filter: 'contrast(100%)',
+                scrollTrigger: {
+                    trigger: el,
+                    start: 'top bottom',
+                    end: 'max',
+                    scrub: true
+                }
+            }, 0);
 
     });
 
@@ -81,6 +81,19 @@ preloadImages('.content__img').then(() => {
     // Once images are preloaded, remove the 'loading' indicator/class from the body
     document.body.classList.remove('loading');
     init();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollAnimation = document.querySelector('.main__action');
+    let scrollThreshold = 100; // Adjust this value to change when the animation disappears
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > scrollThreshold) {
+            scrollAnimation.style.opacity = '0';
+        } else {
+            scrollAnimation.style.opacity = '1';
+        }
+    });
 });
 
 
